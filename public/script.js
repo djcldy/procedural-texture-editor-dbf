c4366e2d874577eddb3563ecaae0360ce0887d1d
Chamal Perera <chamallakshika09@gmail.com>
2022-06-27 09:26:27 +0530

    fix the async loading of all the textures

--- public/script.js
+++ public/script.js
@@ -26,103 +26,108 @@ const ruleText = document.getElementById('ruleText');
 submitBtn.addEventListener('click', submitFacadeRule);
 
 // Execute a function when the user presses a key on the keyboard
-ruleText.addEventListener('keypress', function(event) {
-    // If the user presses the "Enter" key on the keyboard
-    if (event.key === 'Enter') {
-        event.preventDefault(); // Cancel the default action, if needed
-        document.getElementById('submitFacade').click(); // Trigger the button element with a click
-    }
+ruleText.addEventListener('keypress', function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === 'Enter') {
+    event.preventDefault(); // Cancel the default action, if needed
+    document.getElementById('submitFacade').click(); // Trigger the button element with a click
+  }
 });
-
 
 let ele = document.getElementById('sel');
 
 for (let prop in presets) {
-    for (let step in presets[prop]) {
-        ele.innerHTML = ele.innerHTML + '<option value="' + prop + ',' + step + '">' + prop + '-' + step + '</option>';
-    }
+  for (let step in presets[prop]) {
+    ele.innerHTML = ele.innerHTML + '<option value="' + prop + ',' + step + '">' + prop + '-' + step + '</option>';
+  }
 }
 
-sel.onchange = function() {
-    let ele = document.getElementById('sel');
-    let text = ele.value;
-    let id = text.split(',');
-    let settings = presets[id[0]][id[1]];
-    ruleText.value = JSON.stringify(settings, null, 4);
-    submitFacadeRule();
+sel.onchange = function () {
+  let ele = document.getElementById('sel');
+  let text = ele.value;
+  let id = text.split(',');
+  let settings = presets[id[0]][id[1]];
+  ruleText.value = JSON.stringify(settings, null, 4);
+  submitFacadeRule();
 };
 
 init();
 animate();
 
+async function ProceduralTexture(solution, textures, scene) {
+  var start = new Date().getTime(); // benchmark = 482ms
 
+  // let settings = ParseRule(textures[0]) // turns preset string into methods
 
+  let cache = {};
 
+  let parsedTextures = textures.map((o) => ParseRule(o));
 
+  parsedTextures.forEach((o) => {
+    cache[o.name] = null;
+    o['buildingAttributes'] = {
+      totalHeight: 5,
+      floorHeight: 5,
+      totalWidth: 5,
+    };
+  });
 
-function ProceduralTexture(solution, textures, scene) {
+  let material = new THREE.MeshPhongMaterial({
+    bumpScale: 1,
+    envMap: scene.background,
+    reflectivity: 0.3,
+    transparent: true,
+  });
 
-    var start = new Date().getTime(); // benchmark = 482ms 
+  let block = Object.values(solution.blocks)[0];
+  let meshes = TextureBlock(block); // generate facades
+  let mesh = meshes[0];
 
-    // let settings = ParseRule(textures[0]) // turns preset string into methods 
+  mesh.material = material.clone();
 
+  const promiseArray = parsedTextures.map((o) => GenerateTexture(o));
 
-    let cache = {}
+  const resArray = await Promise.all(promiseArray);
 
+  //everything is loaded at this point
 
-    let parsedTextures = textures.map(o => ParseRule(o))
+  resArray.forEach((res, i) => {
+    cache[parsedTextures[i].name] = res;
 
-    parsedTextures.forEach(o => {
+    console.log(cache);
 
-        cache[o.name] = null
-        o['buildingAttributes'] = {
-            totalHeight: 5,
-            floorHeight: 5,
-            totalWidth: 5,
-        };
+    var end = new Date().getTime();
+    var time = end - start;
+    console.log('time...', time, 'ms');
+  });
 
+  //cache is generated at this point
 
-    })
+  //   Promise.all(promiseArray).then((resArray) => {
+  //     resArray.forEach((res, i) => {
+  //       cache[parsedTextures[i].name] = res;
 
-    let material = new THREE.MeshPhongMaterial({
-        bumpScale: 1,
-        envMap: scene.background,
-        reflectivity: 0.3,
-        transparent: true,
-    });
+  //       console.log(cache);
 
-    let block = Object.values(solution.blocks)[0]
-    let meshes = TextureBlock(block); // generate facades 
-    let mesh = meshes[0]
+  //       var end = new Date().getTime();
+  //       var time = end - start;
+  //       console.log('time...', time, 'ms');
+  //     });
+  //   });
 
-    mesh.material = material.clone()
+  //   parsedTextures.forEach((o) => {
+  //     GenerateTexture(o).then((res) => {
+  //       cache[o.name] = res;
 
-    parsedTextures.forEach(o => {
+  //       console.log(cache);
 
-        GenerateTexture(o).then((res) => {
+  //       var end = new Date().getTime();
+  //       var time = end - start;
+  //       console.log('time...', time, 'ms');
+  //     });
+  //   });
 
-            cache[o.name] = res
-
-            console.log(cache)
-
-            var end = new Date().getTime();
-            var time = end - start;
-            console.log('time...', time, 'ms')
-
-
-        });
-
-
-
-    })
-
-
-
-
-
-
-
-    /*    Object.values(solution.blocks).map((block) => {
+  /*    Object.values(solution.blocks).map((block) => {
 
             let meshes = TextureBlock(block); // generate facades 
 
@@ -157,271 +162,235 @@ function ProceduralTexture(solution, textures, scene) {
 
             });
         });*/
-
 }
 
+function GenerateTextures() {}
 
-function GenerateTextures() {
-
-
-
-}
-
-
-function AssignMeshTextures() {
-
-
-}
-
+function AssignMeshTextures() {}
 
 function MergeObjects(obj1, obj2) {
-    let obj = {};
+  let obj = {};
 
-    for (let prop in obj1) {
-        obj[prop] = obj1[prop];
-    }
+  for (let prop in obj1) {
+    obj[prop] = obj1[prop];
+  }
 
-    for (let prop in obj2) {
-        obj[prop] = obj2[prop];
-    }
+  for (let prop in obj2) {
+    obj[prop] = obj2[prop];
+  }
 
-    return obj;
+  return obj;
 }
 
-
-
 function ParseRequest(str) {
-    let object = JSON.parse(str);
+  let object = JSON.parse(str);
 
-    for (let prop in object) {
-        let x = object[prop];
+  for (let prop in object) {
+    let x = object[prop];
 
-        if (isString(x) && prop !== 'name') {
-            console.log('is string!', x);
-            object[prop] = JSON.parse(x);
-        }
+    if (isString(x) && prop !== 'name') {
+      console.log('is string!', x);
+      object[prop] = JSON.parse(x);
     }
+  }
 
-    return object;
+  return object;
 }
 
 function isString(x) {
-    return Object.prototype.toString.call(x) === '[object String]';
+  return Object.prototype.toString.call(x) === '[object String]';
 }
 
-
-
 function AddSlabs() {
-    let material = new THREE.MeshLambertMaterial({ color: 'grey' });
+  let material = new THREE.MeshLambertMaterial({ color: 'grey' });
 
-    Object.values(sampleSolution.blocks).map((block) => {
-        let { geometry } = extrudeBlock({
-            shape: block.shape,
-            holes: [],
-            floors: 1,
-            f2f: 0.5,
-        });
-
-        let floors = block.floors + 1;
-        const matrix = new THREE.Matrix4();
-        const mesh = new THREE.InstancedMesh(geometry, material, floors);
-        mesh.receiveShadow = true;
-        mesh.castShadow = true;
-
-        for (let i = 0; i <= floors; i++) {
-            let y = i * block.f2f + block.translation.y;
-            setMatrix(matrix, 0, y, 0);
-            mesh.setMatrixAt(i, matrix);
-        }
-        scene.add(mesh);
+  Object.values(sampleSolution.blocks).map((block) => {
+    let { geometry } = extrudeBlock({
+      shape: block.shape,
+      holes: [],
+      floors: 1,
+      f2f: 0.5,
     });
+
+    let floors = block.floors + 1;
+    const matrix = new THREE.Matrix4();
+    const mesh = new THREE.InstancedMesh(geometry, material, floors);
+    mesh.receiveShadow = true;
+    mesh.castShadow = true;
+
+    for (let i = 0; i <= floors; i++) {
+      let y = i * block.f2f + block.translation.y;
+      setMatrix(matrix, 0, y, 0);
+      mesh.setMatrixAt(i, matrix);
+    }
+    scene.add(mesh);
+  });
 }
 
 function makeInstanced(geometry) {
-    const matrix = new THREE.Matrix4();
-    const mesh = new THREE.InstancedMesh(geometry, material, api.count);
+  const matrix = new THREE.Matrix4();
+  const mesh = new THREE.InstancedMesh(geometry, material, api.count);
 
-    for (let i = 0; i < api.count; i++) {
-        randomizeMatrix(matrix);
-        mesh.setMatrixAt(i, matrix);
-    }
+  for (let i = 0; i < api.count; i++) {
+    randomizeMatrix(matrix);
+    mesh.setMatrixAt(i, matrix);
+  }
 
-    scene.add(mesh);
+  scene.add(mesh);
 }
 
 function setMatrix(matrix, x, y, z) {
-    const position = new THREE.Vector3();
-    const rotation = new THREE.Euler();
-    const quaternion = new THREE.Quaternion();
-    const scale = new THREE.Vector3();
-    // position.x = Math.random() * 40 - 20;
-    position.y = y;
-    // position.z = Math.random() * 40 - 20;
-    quaternion.setFromEuler(rotation);
-    scale.x = scale.y = scale.z = 1;
-    matrix.compose(position, quaternion, scale);
+  const position = new THREE.Vector3();
+  const rotation = new THREE.Euler();
+  const quaternion = new THREE.Quaternion();
+  const scale = new THREE.Vector3();
+  // position.x = Math.random() * 40 - 20;
+  position.y = y;
+  // position.z = Math.random() * 40 - 20;
+  quaternion.setFromEuler(rotation);
+  scale.x = scale.y = scale.z = 1;
+  matrix.compose(position, quaternion, scale);
 
-    return matrix;
+  return matrix;
 }
 
 function initRequest() {
-    let settings = presets['examples']['texture'];
-    ruleText.value = JSON.stringify(settings, null, 4);
-    submitFacadeRule();
+  let settings = presets['examples']['texture'];
+  ruleText.value = JSON.stringify(settings, null, 4);
+  submitFacadeRule();
 }
 
-function submitFacadeRule() {
+async function submitFacadeRule() {
+  console.log('rule submitted');
+  ruleText.value = ruleText.value.replace(/\s+/g, '');
+  clearScene();
 
-    console.log('rule submitted');
-    ruleText.value = ruleText.value.replace(/\s+/g, '');
-    clearScene();
+  let tex1 = ParseRequest(JSON.stringify(presets['examples']['texture']));
+  let tex2 = ParseRequest(JSON.stringify(presets['office']['90']));
+  let tex3 = ParseRequest(JSON.stringify(presets['office']['80']));
 
+  await ProceduralTexture(sampleSolution, [tex1, tex2, tex3], scene);
 
-    let tex1 = ParseRequest(JSON.stringify(presets['examples']['texture']))
-    let tex2 = ParseRequest(JSON.stringify(presets['office']['90']))
-    let tex3 = ParseRequest(JSON.stringify(presets['office']['80']))
-
-
-
-
-    ;
-
-    ProceduralTexture(sampleSolution, [tex1, tex2, tex3], scene);
-
-    // ProceduralTexture(sampleSolution, [ParseRequest(ruleText.value)], scene);
-    ruleText.value = JSON.stringify(JSON.parse(ruleText.value), null, 4);
-
+  // ProceduralTexture(sampleSolution, [ParseRequest(ruleText.value)], scene);
+  ruleText.value = JSON.stringify(JSON.parse(ruleText.value), null, 4);
 }
 
-
-
-
-
-
-
-
-
-
-
-// do not touch... 
-
+// do not touch...
 
 function setCubeMap() {
-    console.log('setCubeMap');
+  console.log('setCubeMap');
 
-    //cubemap
-    let path = 'textures/clouds/';
-    let format = '.png';
-    let urls = [
-        'textures/clouds/2.png',
-        'textures/clouds/4.png',
-        'textures/clouds/top.png',
-        'textures/clouds/white.png',
-        'textures/clouds/1.png',
-        'textures/clouds/3.png',
-    ];
+  //cubemap
+  let path = 'textures/clouds/';
+  let format = '.png';
+  let urls = [
+    'textures/clouds/2.png',
+    'textures/clouds/4.png',
+    'textures/clouds/top.png',
+    'textures/clouds/white.png',
+    'textures/clouds/1.png',
+    'textures/clouds/3.png',
+  ];
 
-    return new THREE.CubeTextureLoader().load(urls);
+  return new THREE.CubeTextureLoader().load(urls);
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function onDocumentMouseMove(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
 //
 
 function animate() {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    render();
+  render();
 }
 
 function render() {
-    // theta += 0.5
+  // theta += 0.5
 
-    // camera.position.x = radius * Math.sin( THREE.MathUtils.degToRad( theta ) );
-    // camera.position.y = radius * Math.sin( THREE.MathUtils.degToRad( theta ) );
-    // camera.position.z = radius * Math.cos( THREE.MathUtils.degToRad( theta ) );
-    // camera.lookAt( scene.position );
+  // camera.position.x = radius * Math.sin( THREE.MathUtils.degToRad( theta ) );
+  // camera.position.y = radius * Math.sin( THREE.MathUtils.degToRad( theta ) );
+  // camera.position.z = radius * Math.cos( THREE.MathUtils.degToRad( theta ) );
+  // camera.lookAt( scene.position );
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 function initPlane() {
-    let planeGeometry = new THREE.PlaneGeometry(500, 500);
-    planeGeometry.rotateX(-Math.PI / 2);
+  let planeGeometry = new THREE.PlaneGeometry(500, 500);
+  planeGeometry.rotateX(-Math.PI / 2);
 
-    const material = new THREE.MeshStandardMaterial({
-        color: 0xff00ff,
-    });
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xff00ff,
+  });
 
-    let planeMaterial = new THREE.ShadowMaterial();
-    planeMaterial.opacity = 0.5;
+  let planeMaterial = new THREE.ShadowMaterial();
+  planeMaterial.opacity = 0.5;
 
-    let plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.position.y = 0.1;
-    plane.receiveShadow = true;
-    scene.add(plane);
+  let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.position.y = 0.1;
+  plane.receiveShadow = true;
+  scene.add(plane);
 }
 
 function clearScene() {
-    for (let i = scene.children.length - 1; i >= 0; i--) {
-        if (scene.children[i].type === 'Mesh') scene.remove(scene.children[i]);
-    }
+  for (let i = scene.children.length - 1; i >= 0; i--) {
+    if (scene.children[i].type === 'Mesh') scene.remove(scene.children[i]);
+  }
 }
 
-
-
 function init() {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-    const aspect = window.innerWidth / window.innerHeight;
+  container = document.createElement('div');
+  document.body.appendChild(container);
+  const aspect = window.innerWidth / window.innerHeight;
 
-    camera = new THREE.PerspectiveCamera(50, aspect, 0.01, 30000);
-    camera.position.set(0, 0, 25);
-    camera.lookAt(0, 0, 0);
+  camera = new THREE.PerspectiveCamera(50, aspect, 0.01, 30000);
+  camera.position.set(0, 0, 25);
+  camera.lookAt(0, 0, 0);
 
-    scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-    cubeMap = setCubeMap();
+  cubeMap = setCubeMap();
 
-    defaultMaterial = new THREE.MeshPhongMaterial({
-        envMap: cubeMap,
-        reflectivity: 0.3,
-        transparent: true,
-    });
+  defaultMaterial = new THREE.MeshPhongMaterial({
+    envMap: cubeMap,
+    reflectivity: 0.3,
+    transparent: true,
+  });
 
-    scene.background = cubeMap;
+  scene.background = cubeMap;
 
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(1, 1, 1).normalize();
-    scene.add(light);
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(1, 1, 1).normalize();
+  scene.add(light);
 
-    const ambient = new THREE.AmbientLight(0x404040, 2); // soft white light
-    scene.add(ambient);
+  const ambient = new THREE.AmbientLight(0x404040, 2); // soft white light
+  scene.add(ambient);
 
-    raycaster = new THREE.Raycaster();
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
+  raycaster = new THREE.Raycaster();
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement);
 
-    orbit = new OrbitControls(camera, renderer.domElement);
-    orbit.update();
-    orbit.addEventListener('change', render);
+  orbit = new OrbitControls(camera, renderer.domElement);
+  orbit.update();
+  orbit.addEventListener('change', render);
 
-    document.addEventListener('mousemove', onDocumentMouseMove, false);
-    window.addEventListener('resize', onWindowResize, false);
+  document.addEventListener('mousemove', onDocumentMouseMove, false);
+  window.addEventListener('resize', onWindowResize, false);
 
-    initRequest();
+  initRequest();
 }
